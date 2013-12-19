338766c8889d15a58439001caf72d1bf8a6e8b80
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-12-19 12:21:57 -0800

    rebase

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Stanceshare::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stanceshare::Application.config.secret_key_base = '5eb3ed7c4db6ab374367b6914fab780fe253fba2f13cfab8ec05509110b411112c27bc01be1ccb7d278a7db977986c87692b0062fc823a4ab4e539d1ca3ee2f2'
