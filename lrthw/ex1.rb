a18ccbe432dc8bf621d92b36a0e5226decc93d16
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-12-26 12:04:58 -0800

    New user signup & signin

---
+++ lrthw/ex1.rb
@@ -0,0 +1,8 @@
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather 'not'."
+# puts 'I "said" do not touch this.'
+puts "Don't think about it too much, too much, there's no need for us to rush this through."
